106540d6507d050346fc63b828d128d1d6035342
Stefaanb <54105744+stefaanb59@users.noreply.github.com>
2025-07-24 16:46:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# Start Rasa actions server op achtergrond
+echo "Using PORT=${PORT}"
+
+# Start actions server op vaste poort 5055
 rasa run actions --port 5055 &
 
-# Start Rasa server
-rasa run --enable-api --cors "*" --port 5005 --host 0.0.0.0
+# Start Rasa server op poort uit environment variable PORT of fallback 5005
+rasa run --enable-api --cors "*" --port ${PORT:-5005} --host 0.0.0.0
